f9f43bc3fd8211a4281b397a9922b6846fb1dee3
shenaikabilova <shenito93@abv.bg>
2016-12-10 23:51:59 +0200

    Modify login and registration user - connect to server. Added FriendList and ChatRoom activities.

--- MyChat/app/src/main/java/com/kabilova/client/LoginActivity.java
+++ MyChat/app/src/main/java/com/kabilova/client/LoginActivity.java
@@ -5,30 +5,39 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.kabilova.mychat.FriendList;
 import com.kabilova.mychat.R;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
 
+import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
 
 import cz.msebera.android.httpclient.Header;
 
 public class LoginActivity extends AppCompatActivity {
+    final String URL_PATH = "http://10.0.2.2:3000/myapp/login";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        final EditText username = (EditText) findViewById(R.id.editTextUsername);
+        final EditText password = (EditText) findViewById(R.id.editTextPassword);
+
         Button newReg = (Button) findViewById(R.id.buttonNewReg);
         newReg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                RequestParams requestParams = new RequestParams();
-                requestParams.put("username", "aaa");
-                invokeWS(requestParams);
+                Intent regView = new Intent(LoginActivity.this, RegistrationActivity.class);
+                startActivity(regView);
             }
         });
 
@@ -36,26 +45,47 @@ public class LoginActivity extends AppCompatActivity {
         buttonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                RequestParams requestParams = new RequestParams();
+                requestParams.put("username", username.getText().toString());
+                requestParams.put("password", password.getText().toString());
+                invokeWS(requestParams);
             }
         });
     }
 
     public void invokeWS (RequestParams requestParams) {
-
         AsyncHttpClient client = new AsyncHttpClient();
-        client.get("http://10.0.2.2:3000/myapp/myresource", requestParams, new AsyncHttpResponseHandler() {
+        client.get(URL_PATH, requestParams, new AsyncHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                try {
+                    String str = new String(responseBody, "UTF-8");
 
-                    Intent createAccount = new Intent(LoginActivity.this, RegistrationActivity.class);
-                    startActivity(createAccount);
-
+                    JSONObject jsonObject = new JSONObject(str);
+                    if (jsonObject.getBoolean("login")) {
+                        Intent createAccount = new Intent(LoginActivity.this, FriendList.class);
+                        startActivity(createAccount);
+                    }
+                    else {
+                        Toast.makeText(getApplicationContext(), "Invalid username or password!", Toast.LENGTH_LONG).show();
+                    }
+                } catch (UnsupportedEncodingException e) {
+                    Toast.makeText(getApplicationContext(), "UnsupportedEncodingException", Toast.LENGTH_LONG).show();
+                }
+                catch (JSONException e) {
+                    Toast.makeText(getApplicationContext(), "JSONException", Toast.LENGTH_LONG).show();
+                }
             }
 
             @Override
             public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
-
+                if(statusCode == 404) {
+                    Toast.makeText(getApplicationContext(), "Requested resource not found!", Toast.LENGTH_LONG).show();
+                }
+                else if(statusCode == 500) {
+                    Toast.makeText(getApplicationContext(), "Cannot connect to server!", Toast.LENGTH_LONG).show();
+                }
+                else Toast.makeText(getApplicationContext(), "Error!!!", Toast.LENGTH_LONG).show();
             }
         });
     }
--- MyChat/app/src/main/java/com/kabilova/client/RegistrationActivity.java
+++ MyChat/app/src/main/java/com/kabilova/client/RegistrationActivity.java
@@ -5,15 +5,27 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.kabilova.mychat.R;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import cz.msebera.android.httpclient.Header;
 
 public class RegistrationActivity extends AppCompatActivity {
+    final String URL_PATH = "http://10.0.2.2:3000/myapp/registration";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        final EditText username = (EditText) findViewById(R.id.editTextUsername);
+        final EditText pass1 = (EditText) findViewById(R.id.editTextPass1);
+        final EditText pass2 = (EditText) findViewById(R.id.editTextPass2);
 
         Button hasAccount = (Button) findViewById(R.id.buttonHasAccount);
         hasAccount.setOnClickListener(new View.OnClickListener() {
@@ -21,6 +33,40 @@ public class RegistrationActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent backLogin = new Intent(RegistrationActivity.this, LoginActivity.class);
                 startActivity(backLogin);
+            }
+        });
+
+        Button reg = (Button) findViewById(R.id.buttonRegistration);
+        reg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                RequestParams requestParams = new RequestParams();
+                if(pass1.getText().toString().equals(pass2.getText().toString())) {
+                    requestParams.put("username", username.getText().toString());
+                    requestParams.put("password", pass1.getText().toString());
+                    invokeWS(requestParams);
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "Passwords must be the same.", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    private void invokeWS(RequestParams requestParams) {
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.post(URL_PATH, requestParams, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                Intent backLogin = new Intent(RegistrationActivity.this, LoginActivity.class);
+                startActivity(backLogin);
+                Toast.makeText(getApplicationContext(), "Success", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                Toast.makeText(getApplicationContext(), "Fail", Toast.LENGTH_LONG).show();
             }
         });
     }
